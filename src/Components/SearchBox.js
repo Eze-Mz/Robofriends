7cba76d829eac6564278e2d384b43a50d22d5939
Eze Muñoz <eze.munoz@mi.unc.edu.ar>
2022-08-28 00:15:09 -0300

    minor changes: title, scroll component

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-const SearchBox = ({onSearchChange}) => {
-    return (
-        <div className="pa2">
-            <input 
-            className="pa3 ba b--green bg-lightest-blue" 
-            type="search" 
-            placeholder="search robots"
-            onChange={onSearchChange}
-            />
-        </div>
-    );
-}
+const SearchBox = ({ onSearchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search cats"
+        onChange={onSearchChange}
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,53 +1,51 @@
 import React, { Component } from "react";
 import CardList from "../Components/CardList";
-import SearchBox from "../Components/SearchBox"
-import Scroll from "../Components/Scroll"
-import ErrorBoundary from "../Components/ErrorBoundary"
+import SearchBox from "../Components/SearchBox";
+import Scroll from "../Components/Scroll";
+import ErrorBoundary from "../Components/ErrorBoundary";
 import "./App.css";
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ""
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
 
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response=> response.json())
-        .then(users => this.setState({robots: users}));
-    }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-        return !robots.length ?
-            <h1>Loading</h1> :
-            (
-                <React.Fragment>
-                    <header className="tc">
-                        <h1 className="f1">RoboFriends</h1>
-                        <SearchBox onSearchChange={this.onSearchChange} />
-                    </header>
-                    <Scroll>
-                        <main className="tc">
-                            <ErrorBoundary>
-                                <CardList robots={filteredRobots}/>            
-                            </ErrorBoundary>
-                        </main>
-                    </Scroll>
-                </React.Fragment>
-            );    
-        
-    }
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <React.Fragment>
+        <header className="tc">
+          <h1 className="f1">CatFriends</h1>
+          <SearchBox onSearchChange={this.onSearchChange} />
+        </header>
+
+        <main className="tc">
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
